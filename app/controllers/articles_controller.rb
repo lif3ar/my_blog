ef9fb6b5331a59732a2efb4fcb357f328b30b008
Peter <ossipov.pietro@gmail.com>
2016-02-18 17:49:10 +0000

    My blog initial commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,52 @@
+class ArticlesController < ApplicationController
+  
+  def index
+    @articles = Article.all
+  end
+  
+  def new
+    @article = Article.new
+  end
+  
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:success] = "Article has been created!"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    if @article.save
+      flash[:success] = "The article has been updated."
+      render 'show'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.destroy(params[:id])
+    if @article.save
+      flash[:danger] = "Article has been deleted"
+      redirect_to articles_path
+    end
+  end
+  
+  private
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
+end
