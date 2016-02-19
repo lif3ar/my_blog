e34c2363720dc9e0e167e3374fb87e85edb0c9d6
Peter <ossipov.pietro@gmail.com>
2016-02-19 01:10:24 +0000

    Added associations between User and Article

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+    
+    has_many :articles
+    
     validates :username, presence: true, length: {minimum: 3, maximum: 20 }
     validates :email, presence: true, length: {minimum: 6, maximum: 20 } 
+
 end
---
+++ db/migrate/20160219002246_add_reference.rb
@@ -0,0 +1,4 @@
+class AddReference < ActiveRecord::Migration
+  def change
+  end
+end
